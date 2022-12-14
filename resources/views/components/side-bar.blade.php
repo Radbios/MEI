ff7227ddc9324a997fa4418ec919bd9c606c8952
Vitor Magno <vmcg@ic.ufal.br>
2022-12-13 22:26:42 -0300

    quase working

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -1,64 +1,62 @@
-<div>
-    <div id="logo" class="side-item">
-        <img src="../assets/img/brasao-ufal.png" alt="" class="brasao-ufal">
-    </div>
-    <div class="side-list">
-        <ul>
-            <li class="side-item">
-                <a href="" class="link-item">
-                    <i class="bi bi-list"></i>
-                    <div class="item-text">Menu</div>
-                </a>
-            </li>
+<div id="logo" class="side-item">
+    <img src="../assets/img/brasao-ufal.png" alt="" class="brasao-ufal">
+</div>
+<div class="side-list">
+    <ul>
+        <li class="side-item">
+            <a href="" class="link-item">
+                <i class="bi bi-list"></i>
+                <div class="item-text">Menu</div>
+            </a>
+        </li>
 
-            <li class="side-item">
-                <a href="/visaogeral" class="link-item">
-                    <i class="bi bi-graph-up"></i>
-                    <div class="item-text">Visão Geral</div>
-                </a>
-            </li>
+        <li class="side-item">
+            <a href="/visaogeral" class="link-item">
+                <i class="bi bi-graph-up"></i>
+                <div class="item-text">Visão Geral</div>
+            </a>
+        </li>
 
-            <li class="side-item">
-                <a href="/clientes" class="link-item">
-                    <i class="bi bi-person-lines-fill"></i>
-                    <div class="item-text">Clientes</div>
-                </a>
-            </li>
+        <li class="side-item">
+            <a href="/clientes" class="link-item">
+                <i class="bi bi-person-lines-fill"></i>
+                <div class="item-text">Clientes</div>
+            </a>
+        </li>
 
-            <li class="side-item">
-                <a href="/produtos" class="link-item">
-                    <i class="bi bi-clipboard2"></i>
-                    <div class="item-text">Produtos</div>
-                </a>
-            </li>
+        <li class="side-item">
+            <a href="/produtos" class="link-item">
+                <i class="bi bi-clipboard2"></i>
+                <div class="item-text">Produtos</div>
+            </a>
+        </li>
 
-            <li class="side-item">
-                <a href="/encomendas" class="link-item">
-                    <i class="bi bi-bag-fill"></i>
-                    <div class="item-text">Encomendas</div>
-                </a>
-            </li>
+        <li class="side-item">
+            <a href="/encomendas" class="link-item">
+                <i class="bi bi-bag-fill"></i>
+                <div class="item-text">Encomendas</div>
+            </a>
+        </li>
 
-            <li class="side-item">
-                <a href="/estoque" class="link-item">
-                    <i class="bi bi-box-seam"></i>
-                    <div class="item-text">Estoques</div>
-                </a>
-            </li>
+        <li class="side-item">
+            <a href="/estoque" class="link-item">
+                <i class="bi bi-box-seam"></i>
+                <div class="item-text">Estoques</div>
+            </a>
+        </li>
 
-            <li class="side-item">
-                <a href="/vendas" class="link-item">
-                    <i class="bi bi-cart2"></i>
-                    <div class="item-text">Vendas</div>
-                </a>
-            </li>
-        </ul>
-    </div>
+        <li class="side-item">
+            <a href="/vendas" class="link-item">
+                <i class="bi bi-cart2"></i>
+                <div class="item-text">Vendas</div>
+            </a>
+        </li>
+    </ul>
+</div>
 
-    <div id="logout" class="side-item">
-        <a href="/login" class="link-item">
-            <i class="bi bi-door-open"></i>
-            <div class="item-text">Logout</div>
-        </a>
-    </div>
+<div id="logout" class="side-item">
+    <a href="/login" class="link-item">
+        <i class="bi bi-door-open"></i>
+        <div class="item-text">Logout</div>
+    </a>
 </div>
