c83c896cb0062ff9b26b3ab02272fa9659219ab6
Vitor Magno <vmcg@ic.ufal.br>
2022-12-21 23:07:09 -0300

    adjustments

--- resources/views/estoque/edit.blade.php
+++ resources/views/estoque/edit.blade.php
@@ -18,34 +18,34 @@
             </div>
             <div class="mb-3 d-flex flex-row justify-content-between">
                 <div class="mb-3">
-                    <label for="phoneNumber" class="form-label">Fornecedor</label>
+                    <label for="fornecedor" class="form-label">Fornecedor</label>
                     <input type="tel" class="form-control" id="fornecedor" name="fornecedor" value="{{$material->fornecedor}}">
                 </div>
             </div>
             <div class="mb-3">
-                <label for="email" class="form-label">Contato do fornecedor</label>
-                <input type="email" class="form-control" id="contato" name="contato" value="{{$material->contatoFornecedor}}" aria-describedby="emailHelp">
+                <label for="contato" class="form-label">Contato do fornecedor</label>
+                <input type="text" class="form-control" id="contato" name="contato" value="{{$material->contatoFornecedor}}" aria-describedby="emailHelp">
                 <!-- <div id="emailHelp" class="form-text"></div> -->
             </div>
             <div class="mb-3 d-flex flex-row gap-2 justify-content-between">
                 <div class="mb-3">
-                    <label for="city" class="form-label">Lote</label>
+                    <label for="lote" class="form-label">Lote</label>
                     <input type="text" class="form-control" id="lote" name="lote" value="{{$material->lote}}">
                 </div>
                 <div class="mb-3">
-                    <label for="neighborhood" class="form-label">Validade do lot</label>
+                    <label for="validadeLote" class="form-label">Validade do lot</label>
                     <input type="text" class="form-control" id="validadeLote" name="validadeLote" value="{{$material->validadeLote}}">
                 </div>
                 <div class="mb-3">
-                    <label for="street" class="form-label">Preço</label>
+                    <label for="preco" class="form-label">Preço</label>
                     <input type="text" class="form-control" id="preco" name="preco" value="{{$material->price}}">
                 </div>
                 <div class="mb-3">
-                    <label for="number" class="form-label">Unidade</label>
+                    <label for="unidade" class="form-label">Unidade</label>
                     <input type="text" class="form-control" id="unidade" name="unidade" value="{{$material->unit}}">
                 </div>
                 <div class="mb-3">
-                    <label for="cep" class="form-label">Quantidade</label>
+                    <label for="quantidade" class="form-label">Quantidade</label>
                     <input type="text" class="form-control" id="quantidade" name="quantidade" value="{{$material->amount}}">
                 </div>
             </div>
